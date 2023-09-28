a4f63ed613d1011bdaa3cab3fa2cbe921b971aa6
BionicHawk <angel.manzo.rosas@gmail.com>
2023-09-27 20:50:29 -0600

    GPS a tiempo real

--- lib/screens/home/components/meeting_map.dart
+++ lib/screens/home/components/meeting_map.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 
 class MeetingMap extends StatefulWidget {
@@ -11,14 +14,54 @@ class MeetingMap extends StatefulWidget {
 }
 
 class _MeetingMapState extends State<MeetingMap> {
+  LatLng? myPositon;
+  late StreamSubscription<Position> _streamSubscription;
+
+  Future<void> startPositionStream(Function(Position)? callback) async {
+    LocationPermission permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      while (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+      }
+    }
+
+    _streamSubscription = Geolocator.getPositionStream(
+            locationSettings:
+                LocationSettings(accuracy: LocationAccuracy.bestForNavigation))
+        .listen(callback);
+  }
+
+  Future<void> stopPositionStream() async {
+    await _streamSubscription.cancel();
+  }
+
+  void _handlerPositionStream(Position? position) {
+    setState(() {
+      if (position != null) {
+        myPositon = LatLng(position.latitude, position.longitude);
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    startPositionStream(_handlerPositionStream);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    stopPositionStream();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    const myPositon = LatLng(25.725244367892014, -100.3142243727138);
-
     final options = MapOptions(
-      center: myPositon,
+      center: myPositon ?? const LatLng(0, 0),
       minZoom: 5,
-      maxZoom: 25,
+      maxZoom: 18,
       zoom: 18,
     );
 
@@ -32,7 +75,7 @@ class _MeetingMapState extends State<MeetingMap> {
     );
 
     Marker userMarker = Marker(
-        point: myPositon,
+        point: myPositon ?? const LatLng(0, 0),
         builder: (context) => const Icon(
               Icons.person_pin,
               color: Colors.blueAccent,
@@ -43,9 +86,13 @@ class _MeetingMapState extends State<MeetingMap> {
       markers: [userMarker],
     );
 
-    return FlutterMap(
+    FlutterMap map = FlutterMap(
       options: options,
       nonRotatedChildren: [tileLayer, mLayer],
     );
+
+    const loadingWidget = Center(child: CircularProgressIndicator());
+
+    return (myPositon == null) ? loadingWidget : map;
   }
 }
